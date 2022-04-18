835a2b22759cfbad61377be77369fca3726c4ce1
Maël Pasqueraud <mael.pasqueraud@gmail.com>
2022-04-18 15:42:49 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let date1 = new Date();
+
+let dateLocale = date1.toLocaleString('fr-FR',{
+  weekday: 'long',
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric'
+});
+
+document.getElementById('date').innerHTML = 'on est le ' + dateLocale;
